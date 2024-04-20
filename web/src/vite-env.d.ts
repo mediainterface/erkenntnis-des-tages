620310fec1bc588fe9e5dfbb86147cba8830e5ba
TeePlunder <leon.bergmann23@gmail.com>
2024-04-20 14:57:23 +0200

    feat(envs): add types to envs

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_PROJECT_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
