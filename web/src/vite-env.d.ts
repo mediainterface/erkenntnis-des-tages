80df7e5464f218189f81c2bcfb64ff28c8041cb5
TeePlunder <leon.bergmann23@gmail.com>
2024-04-20 15:49:59 +0200

    fix(supabase): move keys

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,13 +1,2 @@
 /// <reference types="vite/client" />
 
-
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_PROJECT_URL: string
-  readonly VITE_SUPABASE_ANON_KEY: string
-  // more env variables...
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
-
