41a4a3f9d0bcf4b0f657970caaec360fe49729a5
Marlelele <marlene.ihle@hotmail.de>
2024-04-21 13:45:07 +0200

    get user from supabase, display as email

--- web/src/common/components/navbar/Navbar.tsx
+++ web/src/common/components/navbar/Navbar.tsx
@@ -1,8 +1,22 @@
+import { supabase } from '@/supabase'
+import { User } from '@supabase/supabase-js'
 import { Avatar, Flex, Typography } from 'antd'
 import gravatar from 'gravatar'
+import React from 'react'
+
 export const Navbar: React.FC = () => {
+  const [user, setUser] = React.useState<User | null>(null)
+  React.useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser()
+      if (data) {
+        setUser(data.user)
+      }
+    }
+    getUser()
+  }, [])
   const gravatarUrl = gravatar.url('mail@jonascurth.de')
-  const userName = 'Marlelele'
+  const userName = user?.email ?? 'keine EMail'
   const greeting = 'Hello'
   const text = `${greeting} ${userName}`
   return (
