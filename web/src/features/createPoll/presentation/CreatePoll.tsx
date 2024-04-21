37c80e5c28c7fa3b203ec75048ab876be7d15745
TeePlunder <leon.bergmann23@gmail.com>
2024-04-21 16:54:23 +0200

    fix(create poll): fix order function

--- web/src/features/createPoll/presentation/CreatePoll.tsx
+++ web/src/features/createPoll/presentation/CreatePoll.tsx
@@ -1,12 +1,12 @@
 import { TABLE_NAME } from '@/common/constants/table-name.constants'
-import { Profile } from '@/common/types/tables/profile.type'
+import { Profile } from '@/common/types/tables/profiles/profile.type'
 import { supabase } from '@/supabase'
 import React from 'react'
 import { EdtInput } from './EdtInput'
 
 const generateOrder = (profiles: Profile[], startPoint: number, lastPoint: number): Profile[] => {
   const order: Profile[] = []
-  for (let i = startPoint; i === lastPoint; i++) {
+  for (let i = startPoint; i < lastPoint; i++) {
     order.push(profiles[i])
   }
 
@@ -18,32 +18,48 @@ const generateOrder = (profiles: Profile[], startPoint: number, lastPoint: numbe
 }
 
 export const CreatePoll: React.FC = () => {
-  const [profiles, SetProfiles] = React.useState<Profile[]>([])
+  const [profiles, setProfiles] = React.useState<Profile[]>([])
 
-  const getProfiles = React.useCallback(async (): Promise<Profile[]> => {
+  const getProfiles = React.useCallback(async () => {
     try {
       // get current user
       const {
         data: { user },
         error: userError,
       } = await supabase.auth.getUser()
+
       if (userError || !user) {
         throw new Error('cannot get current user')
       }
+
       const { data, error: profilesError } = await supabase.from(TABLE_NAME.profiles).select()
+      console.log('data', data)
+
       if (profilesError) {
         throw new Error('cannot get profiles')
       }
-      const profiles = data as Profile[]
+
+      const profilesResponse = data as Profile[]
+
+      console.log('profiles', profilesResponse)
+      console.log(profilesResponse.find((profile) => profile.user_id === user.id))
+
       const startOrderPoint =
-        profiles.filter((profiles) => profiles.order !== -1).find((profile) => profile.user_id === user.id)?.order ?? 0
+        profilesResponse.filter((profiles) => profiles.order_id !== -1).find((profile) => profile.user_id === user.id)
+          ?.order_id ?? 0
 
-      const lastOrderPoint = profiles.reduce((a, b) => {
-        const highestValue = Math.max(a.order, b.order)
-        return a.order === highestValue ? a : b
-      }).order
+      const lastOrderPoint = profilesResponse.reduce((a, b) => {
+        const highestValue = Math.max(a.order_id, b.order_id)
+        return a.order_id === highestValue ? a : b
+      }).order_id
 
-      return generateOrder(profiles, startOrderPoint, lastOrderPoint)
+      console.log(startOrderPoint)
+
+      console.log(lastOrderPoint)
+
+      console.log('order', generateOrder(profilesResponse, startOrderPoint, lastOrderPoint))
+
+      setProfiles(generateOrder(profilesResponse, startOrderPoint, lastOrderPoint))
     } catch (error) {
       alert(error)
     }
@@ -52,12 +68,8 @@ export const CreatePoll: React.FC = () => {
   }, [])
 
   React.useEffect(() => {
-    const performAction = async () => {
-      SetProfiles(await getProfiles())
-    }
-    performAction()
-  }, [getProfiles])
-
+    getProfiles()
+  }, [])
 
   return profiles.map((profile) => <EdtInput profile={profile} />)
 }
