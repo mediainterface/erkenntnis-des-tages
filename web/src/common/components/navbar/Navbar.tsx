a88f9e7b25af906cd3226caffe6ee945da6baf5a
Marlelele <marlene.ihle@hotmail.de>
2024-04-21 12:31:32 +0200

    Username vertically aligned with avatar, greeting added

--- web/src/common/components/navbar/Navbar.tsx
+++ web/src/common/components/navbar/Navbar.tsx
@@ -1,12 +1,15 @@
-import { Avatar, Flex } from 'antd'
+import { Avatar, Flex, Typography } from 'antd'
 import gravatar from 'gravatar'
 export const Navbar: React.FC = () => {
   const gravatarUrl = gravatar.url('mail@jonascurth.de')
   const userName = 'Marlelele'
+  const greeting = 'Hello'
+  const text = `${greeting} ${userName}`
   return (
     <>
       <Flex justify={'flex-end'} align={'flex-start'}>
-        {userName}
+        <Typography style={{ color: 'hotpink', marginTop: '5px', marginRight: '10px' }}>{text}</Typography>
+
         <Avatar src={gravatarUrl} />
       </Flex>
     </>
