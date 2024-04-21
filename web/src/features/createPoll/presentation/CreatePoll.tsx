b675d8c4efa1bcce773184dfea167ff30963528b
TeePlunder <leon.bergmann23@gmail.com>
2024-04-21 17:27:46 +0200

    feat(create poll): get inputs from childs

--- web/src/features/createPoll/presentation/CreatePoll.tsx
+++ web/src/features/createPoll/presentation/CreatePoll.tsx
@@ -1,16 +1,20 @@
 import { TABLE_NAME } from '@/common/constants/table-name.constants'
 import { Profile } from '@/common/types/tables/profiles/profile.type'
 import { supabase } from '@/supabase'
+import { Button, Spin } from 'antd'
 import React from 'react'
-import { EdtInput } from './EdtInput'
+
+
+import { EdtInput, EdtInputHandle } from './EdtInput'
 
 const generateOrder = (profiles: Profile[], startPoint: number, lastPoint: number): Profile[] => {
   const order: Profile[] = []
-  for (let i = startPoint; i < lastPoint; i++) {
+  console.log(profiles)
+  for (let i = startPoint - 1; i <= lastPoint - 1; i++) {
     order.push(profiles[i])
   }
 
-  for (let i = 0; i < startPoint; i++) {
+  for (let i = 0; i < startPoint - 1; i++) {
     order.push(profiles[i])
   }
 
@@ -33,16 +37,12 @@ export const CreatePoll: React.FC = () => {
       }
 
       const { data, error: profilesError } = await supabase.from(TABLE_NAME.profiles).select()
-      console.log('data', data)
 
       if (profilesError) {
         throw new Error('cannot get profiles')
       }
 
       const profilesResponse = data as Profile[]
-
-      console.log('profiles', profilesResponse)
-      console.log(profilesResponse.find((profile) => profile.user_id === user.id))
 
       const startOrderPoint =
         profilesResponse.filter((profiles) => profiles.order_id !== -1).find((profile) => profile.user_id === user.id)
@@ -52,12 +52,6 @@ export const CreatePoll: React.FC = () => {
         const highestValue = Math.max(a.order_id, b.order_id)
         return a.order_id === highestValue ? a : b
       }).order_id
-
-      console.log(startOrderPoint)
-
-      console.log(lastOrderPoint)
-
-      console.log('order', generateOrder(profilesResponse, startOrderPoint, lastOrderPoint))
 
       setProfiles(generateOrder(profilesResponse, startOrderPoint, lastOrderPoint))
     } catch (error) {
@@ -71,6 +65,22 @@ export const CreatePoll: React.FC = () => {
     getProfiles()
   }, [])
 
-  return profiles.map((profile) => <EdtInput profile={profile} />)
+  const childRefs = React.useRef<EdtInputHandle[]>([])
+
+  const handleClick = () => {
+    const allChildData = childRefs.current.map((ref) => ref.getEdtInput())
+    console.log('Data from all children:', allChildData)
+  }
+
+  return profiles.length === 0 ? (
+    <Spin size="large" />
+  ) : (
+    <>
+      {profiles.map((profile, index) => (
+        <EdtInput profile={profile} style={{ marginTop: '20px' }} ref={(el) => (childRefs.current[index] = el!)} />
+      ))}
+      <Button onClick={handleClick}>get data</Button>
+    </>
+  )
 }
 
--- web/src/features/createPoll/presentation/EdtInput.tsx
+++ web/src/features/createPoll/presentation/EdtInput.tsx
@@ -1,20 +1,34 @@
 import { Profile } from '@/common/types/tables/profiles/profile.type'
 import { Avatar, Flex } from 'antd'
 import Input from 'antd/es/input/Input'
+import React, { useImperativeHandle } from 'react'
 
 type EdtInputProps = {
   profile: Profile
+  style?: React.CSSProperties
 }
 
-export const EdtInput: React.FC<EdtInputProps> = (props) => {
-  const { profile } = props
-  return (
-    <>
-      <Flex justify={'space-evenly'} align={'center'}>
-        <Avatar src={profile.avatar_url} />
-        <Input placeholder="Icons" />
-      </Flex>
-    </>
-  )
+export type EdtInputHandle = {
+  getEdtInput: () => string
 }
+
+export const EdtInput = React.forwardRef<EdtInputHandle, EdtInputProps>((props, ref) => {
+  const { profile, style } = props
+  const [edtInput, setEdtInput] = React.useState('')
+
+  const handleEdtInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEdtInput(event.target.value)
+  }
+
+  useImperativeHandle(ref, () => ({
+    getEdtInput: () => edtInput,
+  }))
+
+  return (
+    <Flex justify={'space-evenly'} align={'center'} style={style}>
+      <Avatar src={profile.avatar_url} />
+      <Input placeholder="Icons" value={edtInput} onChange={handleEdtInputChange} />
+    </Flex>
+  )
+})
 
