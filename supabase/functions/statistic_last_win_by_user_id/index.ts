9e2e17ea0ecee11647c5e02a9413c255cfbedbc1
marcelatmi <schmidt@mediainterface.de>
2024-04-21 17:32:26 +0200

    Update Edgefunction for LAST WIN BY USER ID

--- supabase/functions/statistic_last_win_by_user_id/index.ts
+++ supabase/functions/statistic_last_win_by_user_id/index.ts
@@ -2,6 +2,26 @@ import "https://esm.sh/@supabase/supabase-js@2";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
 import { corsHeaders } from "../_shared/cors.ts";
 
+const getLastWinByUserIdFromSupabase = async (req: Request, userId: string): Promise<Date> => {
+    const supabaseClient = createClient(
+        Deno.env.get("SUPABASE_URL") ?? "",
+        Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+        {
+            global: {
+                headers: { Authorization: req.headers.get("Authorization")! },
+            },
+        },
+    );
+
+    const { data, error } = await supabaseClient.functions.invoke('statistic_last_win_by_user_id', {
+        body: JSON.stringify({ user_id: userId })
+    })
+
+    if (error) throw error;
+
+    return data;
+}
+
 Deno.serve(async (req) => {
     // This is needed to invoke the function from a browser.
     if (req.method === "OPTIONS") {
@@ -11,7 +31,9 @@ Deno.serve(async (req) => {
     const { userId } = await req.json();
 
     try {
-        return new Response(JSON.stringify({ date: "22-04-2024" }), {
+        const lastWinDate: Date = await getLastWinByUserIdFromSupabase(req, userId);
+
+        return new Response(JSON.stringify({ date: lastWinDate }), {
             headers: { ...corsHeaders, "Content-Type": "application/json" },
             status: 200,
         });
