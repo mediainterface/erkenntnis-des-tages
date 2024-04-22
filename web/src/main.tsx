822eeecc31430f3df678eef960d6ad01612998fa
Moeller Carolin <carolin.moeller@mediainterface.de>
2024-04-22 08:46:26 +0200

    feat(feat-antd): theme auslagern

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,5 @@
-import { ConfigProvider, theme } from 'antd'
+import { customTheme } from '@/theme.tsx'
+import { ConfigProvider } from 'antd'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -6,23 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider
-      theme={{
-        cssVar: true,
-        algorithm: theme.darkAlgorithm,
-        token: {
-          colorPrimary: 'hsl(265, 80%, 58%)',
-          colorBgBase: '#110f17',
-
-          fontSizeHeading1: 28,
-          fontSizeHeading2: 20,
-          fontSizeHeading3: 18,
-          fontSizeHeading4: 16,
-          fontSizeHeading5: 14,
-          fontWeightStrong: 300,
-        },
-      }}
-    >
+    <ConfigProvider theme={customTheme}>
       <App />
     </ConfigProvider>
   </React.StrictMode>,
---
+++ web/src/theme.tsx
@@ -0,0 +1,17 @@
+import { theme } from 'antd'
+
+export const customTheme = {
+  cssVar: true,
+  algorithm: theme.darkAlgorithm,
+  token: {
+    colorPrimary: 'hsl(265, 80%, 58%)',
+    colorBgBase: '#110f17',
+
+    fontSizeHeading1: 28,
+    fontSizeHeading2: 20,
+    fontSizeHeading3: 18,
+    fontSizeHeading4: 16,
+    fontSizeHeading5: 14,
+    fontWeightStrong: 300,
+  },
+}
