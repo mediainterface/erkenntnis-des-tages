b904e8226b979dcd726d5e63a6ef28d319ab0f10
TeePlunder <leon.bergmann23@gmail.com>
2024-04-29 15:38:38 +0200

    feat(close poll): navigate to home after closing poll

--- web/src/features/result/presentation/ClosePoll.tsx
+++ web/src/features/result/presentation/ClosePoll.tsx
@@ -1,9 +1,11 @@
 import { TABLE_NAME } from '@/common/constants/table-name.constants'
 import { Poll } from '@/common/types/tables/polls/poll.type'
+import { ROUTING_PATH } from '@/features/router/domain/constants/routing-path.constants'
 import { useUserStore } from '@/stores/user.store'
 import { supabase } from '@/supabase'
 import { Button, message } from 'antd'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 type ClosePollProps = Pick<Poll, 'id'>
 
@@ -12,6 +14,8 @@ export const ClosePoll: React.FC<ClosePollProps> = (props) => {
   const [isPollCreator, setIsPollCreator] = React.useState(false)
   const user = useUserStore((state) => state.user)
   const [messageApi] = message.useMessage()
+  const [hasClickedClose, setHasClickedClose] = React.useState(false)
+  const navigate = useNavigate()
 
   const getPollCreator = React.useCallback(async () => {
     const { data, error } = await supabase.from(TABLE_NAME.polls).select().eq('id', id).maybeSingle()
@@ -28,14 +32,23 @@ export const ClosePoll: React.FC<ClosePollProps> = (props) => {
   }, [getPollCreator])
 
   const handleClosePollClick = async () => {
+    setHasClickedClose(true)
     const { error } = await supabase.from(TABLE_NAME.polls).update({ is_closed: true }).eq('id', id)
     if (error) {
       messageApi.error({ content: 'Umfrage konnte nicht geschlossen werden' })
-      return
+    } else {
+      messageApi.success({ content: 'Umfrage wurde erfolgreich geschlossen' })
     }
-    messageApi.success({ content: 'Umfrage wurde erfolgreich geschlossen' })
+    setHasClickedClose(false)
+    navigate(ROUTING_PATH.home)
   }
 
-  return isPollCreator && <Button onClick={handleClosePollClick}>Umfrage schließen</Button>
+  return (
+    isPollCreator && (
+      <Button onClick={handleClosePollClick} loading={hasClickedClose}>
+        Umfrage schließen
+      </Button>
+    )
+  )
 }
 
