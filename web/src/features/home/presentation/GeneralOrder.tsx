3c9f8552a1ff23991777a4b164cd2cb5e98eb23b
TeePlunder <leon.bergmann23@gmail.com>
2024-05-28 19:43:10 +0200

    feet(general order): display a circle with more circles around the creacte poll button

--- web/src/features/home/presentation/GeneralOrder.tsx
+++ web/src/features/home/presentation/GeneralOrder.tsx
@@ -1,18 +1,61 @@
-export const GeneralOrder: React.FC = () => {
+interface CircleProps {
+  x: number
+  y: number
+  radius: number
+}
+
+const Circle: React.FC<CircleProps> = ({ x, y, radius }) => (
+  <div
+    className="circle"
+    style={{
+      width: radius * 2,
+      height: radius * 2,
+      borderRadius: '50%',
+      position: 'absolute',
+      left: x - radius,
+      top: y - radius,
+      border: '1px solid white',
+    }}
+  ></div>
+)
+
+interface CircleArrangementProps {
+  largeRadius: number
+  smallRadius: number
+  smallCircleCount: number
+  centerX: number
+  centerY: number
+}
+
+export const GeneralOrder: React.FC<CircleArrangementProps> = () => {
+  const largeRadius = 500
+  const smallRadius = 100
+  const smallCircleCount = 10
+  const centerX = 110
+  const centerY = 110
+  const circles = []
+  const angleStep = (2 * Math.PI) / smallCircleCount
+
+  for (let i = 0; i < smallCircleCount; i++) {
+    const angle = i * angleStep
+    const x = centerX + largeRadius * Math.cos(angle)
+    const y = centerY + largeRadius * Math.sin(angle)
+    circles.push(<Circle key={i} x={x} y={y} radius={smallRadius} />)
+  }
+
   return (
-    <>
-      <span
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          width: '50%',
-          height: '80%',
-          borderRadius: '100%',
-          border: '1px solid white',
-          transform: 'translate(-50%, -50%)',
-        }}
-      />
-    </>
+    <div
+      style={{
+        width: 2 * (largeRadius + smallRadius),
+        height: 2 * (largeRadius + smallRadius),
+        position: 'absolute',
+        zIndex: 0,
+        top: '42%',
+        left: '45%',
+      }}
+    >
+      <Circle x={centerX} y={centerY} radius={largeRadius} />
+      {circles}
+    </div>
   )
 }
