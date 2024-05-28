bb4c6673ff4ef2785eb78fd2c9d463f76d98035d
TeePlunder <leon.bergmann23@gmail.com>
2024-05-28 18:29:30 +0200

    feat(home): center button and icon

--- web/src/features/home/presentation/Home.tsx
+++ web/src/features/home/presentation/Home.tsx
@@ -2,7 +2,7 @@ import { PERSON_X_EMAIL } from '@/common/constants/person-x-email.constants'
 import { ROUTING_PATH } from '@/features/router/domain/constants/routing-path.constants'
 import { supabase } from '@/supabase.tsx'
 import { User } from '@supabase/supabase-js'
-import { Button, Card, Flex, Typography } from 'antd'
+import { Button, Flex, Typography } from 'antd'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -29,17 +29,14 @@ export const Home: React.FC = () => {
   const greeting = user?.email === PERSON_X_EMAIL ? 'Bonjour' : 'Hello'
   const greetingText = `${greeting} ${userName}`
   return (
-    <Card>
-      <Flex vertical align={'center'} gap={'large'}>
-        <Typography.Text>{greetingText}</Typography.Text>
-        <div style={{ maxWidth: '250px' }}>
-          <img src="https://cdn-icons-png.flaticon.com/128/3468/3468306.png" alt={'unicorn'} />
-        </div>
-        <Button type={'primary'} onClick={handleCreatePoll}>
-          Umfrage erstellen
-        </Button>
-      </Flex>
-    </Card>
+    <Flex vertical align={'center'} justify="center" gap="large" style={{ height: '100%' }}>
+      <Typography.Text>{greetingText}</Typography.Text>
+      <div style={{ maxWidth: '250px' }}>
+        <img src="https://cdn-icons-png.flaticon.com/128/3468/3468306.png" alt={'unicorn'} />
+      </div>
+      <Button type={'primary'} onClick={handleCreatePoll}>
+        Umfrage erstellen
+      </Button>
+    </Flex>
   )
 }
-
