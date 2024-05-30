28dd060d284109e004b8c3fb777202c3b0c5794b
TeePlunder <leon.bergmann23@gmail.com>
2024-05-30 21:56:13 +0200

    feat(general order): first one at the top

--- web/src/features/home/presentation/GeneralOrder.tsx
+++ web/src/features/home/presentation/GeneralOrder.tsx
@@ -1,23 +1,34 @@
-interface CircleProps {
+import { TABLE_NAME } from '@/common/constants/table-name.constants'
+import { Profile } from '@/common/types/tables/profiles/profile.type'
+import { supabase } from '@/supabase'
+import { Tooltip } from 'antd'
+import React from 'react'
+
+type CircleProps = {
   x: number
   y: number
   radius: number
-}
+} & Partial<Profile>
 
-const Circle: React.FC<CircleProps> = ({ x, y, radius }) => (
-  <div
-    className="circle"
-    style={{
-      width: radius * 2,
-      height: radius * 2,
-      borderRadius: '50%',
-      position: 'absolute',
-      left: x - radius,
-      top: y - radius,
-      border: '1px solid white',
-    }}
-  ></div>
-)
+const Circle: React.FC<CircleProps> = (props) => {
+  const { x, y, radius, avatar_url, username } = props
+  return (
+    <Tooltip title={username}>
+      <img
+        src={avatar_url}
+        style={{
+          width: radius * 2,
+          height: radius * 2,
+          borderRadius: '50%',
+          position: 'absolute',
+          left: x - radius,
+          top: y - radius,
+          border: '1px solid white',
+        }}
+      />
+    </Tooltip>
+  )
+}
 
 interface GeneralOrderProps {
   size: number
@@ -25,6 +36,7 @@ interface GeneralOrderProps {
 
 export const GeneralOrder: React.FC<GeneralOrderProps> = (props) => {
   const { size } = props
+  const [profiles, setProfiles] = React.useState<Profile[]>([])
   const largeRadius = size
   const smallRadius = size / 6
   const smallCircleCount = 10
@@ -33,11 +45,30 @@ export const GeneralOrder: React.FC<GeneralOrderProps> = (props) => {
   const circles = []
   const angleStep = (2 * Math.PI) / smallCircleCount
 
-  for (let i = 0; i < smallCircleCount; i++) {
-    const angle = i * angleStep
-    const x = centerX + largeRadius * Math.cos(angle)
-    const y = centerY + largeRadius * Math.sin(angle)
-    circles.push(<Circle key={i} x={x} y={y} radius={smallRadius} />)
+  React.useEffect(() => {
+    const getProfiles = async () => {
+      const { data, error: profilesError } = await supabase.from(TABLE_NAME.profiles).select()
+
+      if (profilesError) {
+        throw new Error('cannot get profiles')
+      }
+
+      const sortedProfiles = (data as Profile[])
+        .filter((profiles) => profiles.order_id !== -1)
+        .sort((a, b) => a.order_id - b.order_id)
+      setProfiles(sortedProfiles)
+    }
+    getProfiles()
+  }, [])
+
+  if (profiles.length) {
+    for (let i = 0; i < smallCircleCount; i++) {
+      const angle = i * angleStep - 1.6
+      const x = centerX + largeRadius * Math.cos(angle)
+      const y = centerY + largeRadius * Math.sin(angle)
+      // TODO: mark first circle
+      circles.push(<Circle key={i} x={x} y={y} radius={smallRadius} {...profiles[i]} />)
+    }
   }
 
   return (
