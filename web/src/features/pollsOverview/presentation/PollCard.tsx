f96d6b0da37ac33d8c8315b3bcffdd41c3529da2
TeePlunder <leon.bergmann23@gmail.com>
2024-07-01 11:53:39 +0200

    feat(poll card): add avatar before date to see who has created the poll

--- web/src/features/pollsOverview/presentation/PollCard.tsx
+++ web/src/features/pollsOverview/presentation/PollCard.tsx
@@ -1,8 +1,12 @@
 import { DateFormat } from '@/common/components/date/DateFormat'
+import { TABLE_NAME } from '@/common/constants/table-name.constants'
 import { Poll } from '@/common/types/tables/polls/poll.type'
+import { Profile } from '@/common/types/tables/profiles/profile.type'
 import { ROUTING_PATH } from '@/features/router/domain/constants/routing-path.constants'
+import { supabase } from '@/supabase'
 import { FormOutlined, PieChartOutlined } from '@ant-design/icons'
-import { Button, Card, Flex, Tooltip } from 'antd'
+import { Avatar, Button, Card, Flex, Tooltip } from 'antd'
+import React from 'react'
 import { generatePath, useNavigate } from 'react-router-dom'
 
 type PollCardProps = {
@@ -17,12 +21,28 @@ export const PollCard: React.FC<PollCardProps> = (props) => {
     navigate(generatePath(poll.is_closed ? ROUTING_PATH.result : ROUTING_PATH.vote, { pollId: poll.id }))
   }
 
+  const [profile, setProfile] = React.useState<Profile | null>(null)
+
+  const getUser = React.useCallback(async () => {
+    const { data } = await supabase.from(TABLE_NAME.profiles).select().eq('user_id', poll.user_id).maybeSingle()
+    if (data) {
+      setProfile(data as Profile)
+    }
+  }, [poll.user_id])
+
+  React.useEffect(() => {
+    getUser()
+  }, [getUser])
+
   return (
     <Card
       style={{ width: '300px', borderColor: poll.is_closed ? 'var(--ant-color-primary-active)' : 'inherit' }}
       key={poll.id}
     >
       <Flex justify="space-between" align="center">
+        <Tooltip title={profile?.username}>
+          <Avatar size="large" src={profile?.avatar_url} />
+        </Tooltip>
         <DateFormat date={new Date(poll.created_at)} />
         <Tooltip title={poll.is_closed ? 'Ergebnisse' : 'Abstimmen'}>
           <Button
