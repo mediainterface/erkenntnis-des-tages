0b84001a801a3c83d65af6b132b64e4eb0c46c5a
Leon Bergmann <93575165+TeePlunder@users.noreply.github.com>
2024-07-02 09:16:14 +0200

    31 wer hat angefangen (#41)
    * feat(poll card): add avatar before date to see who has created the poll
    
    * fix #40 : wrong format option for minutes & parseISO to get the date object
    
    * fix(poll card): place handlePollClick under the useEffect

--- web/src/features/pollsOverview/presentation/PollCard.tsx
+++ web/src/features/pollsOverview/presentation/PollCard.tsx
@@ -1,8 +1,13 @@
 import { DateFormat } from '@/common/components/date/DateFormat'
+import { TABLE_NAME } from '@/common/constants/table-name.constants'
 import { Poll } from '@/common/types/tables/polls/poll.type'
+import { Profile } from '@/common/types/tables/profiles/profile.type'
 import { ROUTING_PATH } from '@/features/router/domain/constants/routing-path.constants'
+import { supabase } from '@/supabase'
 import { FormOutlined, PieChartOutlined } from '@ant-design/icons'
-import { Button, Card, Flex, Tooltip } from 'antd'
+import { Avatar, Button, Card, Flex, Tooltip } from 'antd'
+import { parseISO } from 'date-fns'
+import React from 'react'
 import { generatePath, useNavigate } from 'react-router-dom'
 
 type PollCardProps = {
@@ -12,6 +17,19 @@ type PollCardProps = {
 export const PollCard: React.FC<PollCardProps> = (props) => {
   const { poll } = props
   const navigate = useNavigate()
+
+  const [profile, setProfile] = React.useState<Profile | null>(null)
+
+  const getUser = React.useCallback(async () => {
+    const { data } = await supabase.from(TABLE_NAME.profiles).select().eq('user_id', poll.user_id).maybeSingle()
+    if (data) {
+      setProfile(data as Profile)
+    }
+  }, [poll.user_id])
+
+  React.useEffect(() => {
+    getUser()
+  }, [getUser])
 
   const handlePollClick = () => {
     navigate(generatePath(poll.is_closed ? ROUTING_PATH.result : ROUTING_PATH.vote, { pollId: poll.id }))
@@ -23,7 +41,10 @@ export const PollCard: React.FC<PollCardProps> = (props) => {
       key={poll.id}
     >
       <Flex justify="space-between" align="center">
-        <DateFormat date={new Date(poll.created_at)} />
+        <Tooltip title={profile?.username}>
+          <Avatar size="large" src={profile?.avatar_url} />
+        </Tooltip>
+        <DateFormat date={parseISO(poll.created_at)} />
         <Tooltip title={poll.is_closed ? 'Ergebnisse' : 'Abstimmen'}>
           <Button
             type="primary"
