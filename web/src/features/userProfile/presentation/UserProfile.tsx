772d79d5b93d164d763da06c5890d6d9fe55ef2d
TeePlunder <leon.bergmann23@gmail.com>
2024-09-13 01:27:16 +0200

    feat(user profile): use description component to display username

--- web/src/features/userProfile/presentation/UserProfile.tsx
+++ web/src/features/userProfile/presentation/UserProfile.tsx
@@ -1,25 +1,31 @@
 import { useUserStore } from '@/stores/useUserStore.tsx'
-import { Card, Flex, Typography } from 'antd'
+import { Card, Descriptions, DescriptionsProps, Flex } from 'antd'
 import Avatar from 'antd/es/avatar/avatar'
 import React from 'react'
 
 export const UserProfile: React.FC = () => {
   const userProfile = useUserStore((state) => state.userProfile)
 
-  if (!userProfile) return null
-  console.log(userProfile)
+  if (!userProfile) {
+    return null
+  }
+
+  const items: DescriptionsProps['items'] = [
+    {
+      key: '1',
+      label: 'UserName',
+      children: userProfile.username,
+    },
+  ]
 
   return (
     <Flex style={{ justifyContent: 'center', alignItems: 'start', paddingTop: '5rem' }}>
       <Card style={{ width: 300 }}>
         <Flex align="center" gap={'large'}>
-          <Avatar size={80} src={userProfile?.avatar_url} />
-          <Flex vertical gap={'middle'}>
-            <Flex gap={'large'}>
-              <Typography.Text>Username:</Typography.Text>
-              <Typography.Text>{userProfile.username}</Typography.Text>
-            </Flex>
-          </Flex>
+          <div>
+            <Avatar size={80} src={userProfile.avatar_url} />
+          </div>
+          <Descriptions items={items} title="Infos" />
         </Flex>
       </Card>
     </Flex>
