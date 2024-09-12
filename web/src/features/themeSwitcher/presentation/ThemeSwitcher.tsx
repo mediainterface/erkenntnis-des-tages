a2696a120c6f6d0b205d0831cd988da8085d625b
TeePlunder <leon.bergmann23@gmail.com>
2024-09-12 14:12:15 +0200

    feat(button): add excape button that runs way from your cursor

--- web/src/features/themeSwitcher/presentation/ThemeSwitcher.tsx
+++ web/src/features/themeSwitcher/presentation/ThemeSwitcher.tsx
@@ -1,89 +1,145 @@
-import { useWindowSize } from '@/common/hooks/useWindowSize'
-import { Button } from 'antd'
 import React from 'react'
 
 export const ThemeSwitcher: React.FC = () => {
-  const { height, width } = useWindowSize()
-  const pagePadding = 50
+  // Inner threshold wrapper to snap the button to a new position
+  const ref = React.useRef<HTMLDivElement>(null)
 
-  const [buttonPosition, setButtonPosition] = React.useState({ x: width + pagePadding, y: height + pagePadding })
-  const [mousePos, setMousePos] = React.useState({ x: 0, y: 0 })
-  const [velocity, setVelocity] = React.useState({ vx: 0, vy: 0 })
-  const [isMouseInside, setIsMouseInside] = React.useState(false)
+  // Outer wrapper to move the button away from the cursor
+  const outerRef = React.useRef<HTMLDivElement>(null)
 
-  const buttonRef = React.useRef<HTMLButtonElement>(null)
+  // Snap offset for the button
+  const [x, setX] = React.useState(0)
+  const [y, setY] = React.useState(0)
 
-  // Ref to store the previous mouse position and timestamp
-  const prevPosRef = React.useRef({ x: 0, y: 0, timestamp: Date.now() })
+  // Slow move offset for the button
+  const [outerX, setOuterX] = React.useState(0)
+  const [outerY, setOuterY] = React.useState(0)
 
-  const handleMouseMove = (event: MouseEvent) => {
-    const { clientX, clientY } = event
-    const currentTime = Date.now()
-    const timeDiff = currentTime - prevPosRef.current.timestamp
+  // For disabling the button when it's moving
+  const [disabled, setDisabled] = React.useState(false)
 
-    if (timeDiff > 0) {
-      const deltaX = clientX - prevPosRef.current.x
-      const deltaY = clientY - prevPosRef.current.y
-
-      // Calculate velocity
-      const vx = deltaX / timeDiff
-      const vy = deltaY / timeDiff
-
-      setVelocity({ vx, vy })
-      setMousePos({ x: clientX, y: clientY })
-
-      // Update the previous position and timestamp
-      prevPosRef.current = { x: clientX, y: clientY, timestamp: currentTime }
-    }
+  const randomBetween = (min: number, max: number) => {
+    return Math.random() * (max - min) + min
   }
 
-  const updateButtonPosition = () => {
-    setButtonPosition((prev) => {
-      return { x: prev.x + 10, y: prev.y + 10 }
-    })
+  // Will slowly move the button away from the cursor when close
+  const onOuterMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!outerRef.current) {
+      return
+    }
+
+    const outerRect = outerRef.current.getBoundingClientRect()
+    const innerRect = ref.current?.getBoundingClientRect()
+    const wrapperRect = ref.current?.parentElement?.parentElement?.getBoundingClientRect()
+
+    // if innerRect is outside of wrapper, call onMouseEnter
+    if (!innerRect || !wrapperRect) {
+      return
+    }
+    if (
+      innerRect?.left < wrapperRect?.left ||
+      innerRect?.right > wrapperRect?.right ||
+      innerRect?.top < wrapperRect?.top ||
+      innerRect?.bottom > wrapperRect?.bottom
+    ) {
+      onMouseEnter(e)
+      return
+    }
+
+    // Calculate the center of the element
+    const centerX = outerRect.left + outerRect.width / 2
+    const centerY = outerRect.top + outerRect.height / 2
+
+    // Calculate the distance from the cursor to the center of the element
+    const distanceX = e.clientX - centerX
+    const distanceY = e.clientY - centerY
+
+    // Define a fraction of the distance to offset with
+    const moveY = -distanceY / 140
+    const moveX = -distanceX / 140
+
+    // Update the element's position
+    setOuterX((prevX) => prevX + moveX)
+    setOuterY((prevY) => prevY + moveY)
   }
 
-  React.useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove)
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove)
+  // Will reposition the mouse enters some threshold
+  const onMouseEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (disabled) {
+      return
     }
-  }, [])
+
+    setDisabled(true)
+
+    setTimeout(() => {
+      setDisabled(false)
+    }, 80)
+
+    if (!ref.current) {
+      return
+    }
+
+    const buttonRect = ref.current.getBoundingClientRect()
+    const wrapperRect = ref.current.parentElement?.parentElement?.getBoundingClientRect()
+
+    if (!wrapperRect || !buttonRect) {
+      return
+    }
+
+    const cursorX = e.clientX - wrapperRect.left
+    const cursorY = e.clientY - wrapperRect.top
+
+    // Generate 10 random positions inside the wrapper
+    // and keep track of the farthest one
+    let maxDistance = 0
+    let farthestPosition = { x: 0, y: 0 }
+    const visableXPositions = { min: 450, max: 1300 }
+    for (let i = 0; i < 10; i++) {
+      const randomX = randomBetween(visableXPositions.min, visableXPositions.max)
+      const randomY = randomBetween(buttonRect.height / 2, wrapperRect.height - buttonRect.height / 2)
+
+      const distance = Math.sqrt(Math.pow(randomX - cursorX, 2) + Math.pow(randomY - cursorY, 2))
+      if (distance > maxDistance) {
+        maxDistance = distance
+        farthestPosition = { x: randomX, y: randomY }
+        console.log(farthestPosition)
+      }
+    }
+
+    // Set the button's translation to the farthest position
+    setX(farthestPosition.x - wrapperRect.width / 2)
+    setY(farthestPosition.y - wrapperRect.height / 2)
+
+    // Reset the slow move offset covered next
+    setOuterX(0)
+    setOuterY(0)
+  }
 
   return (
     <div
-      onMouseLeave={() => {
-        setIsMouseInside(false)
-      }}
-      onMouseEnter={() => {
-        setIsMouseInside(true)
-      }}
-      onMouseMove={() => {
-        isMouseInside && updateButtonPosition()
-      }}
       style={{
-        position: 'absolute',
-        right: buttonPosition.x,
-        bottom: buttonPosition.y,
-        backgroundColor: isMouseInside ? 'hotpink' : 'lightblue',
-        padding: '100px',
+        display: 'inline-block',
+        padding: '16px',
+        pointerEvents: disabled ? 'none' : 'auto',
+        opacity: disabled ? 0.05 : 1,
+        transform: `translate3d(${outerX + x}px, ${outerY + y}px, 0px)`,
+        transition: 'transform 120ms ease',
       }}
+      onMouseMove={onOuterMove}
+      ref={outerRef}
     >
-      <p>
-        Mouse Position: ({mousePos.x}, {mousePos.y})
-      </p>
-      <p>
-        Mouse Velocity: (vx: {velocity.vx.toFixed(2)}, vy: {velocity.vy.toFixed(2)}) px/ms
-      </p>
-      <Button
-        ref={buttonRef}
+      {/*TODO: display tooltip with "sry Henri :)"*/}
+      <div
+        ref={ref}
         style={{
-          transition: 'left 0.3s, top 0.3s', // for smooth transition
+          display: 'inline-block',
+          padding: '30px',
         }}
+        onMouseEnter={onMouseEnter}
+        onMouseMove={onMouseEnter}
       >
-        Move Me
-      </Button>
+        <button>Click Me</button>
+      </div>
     </div>
   )
 }
