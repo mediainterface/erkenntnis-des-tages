c6cb562d75a22c96450141b9e06614f331770a86
TeePlunder <leon.bergmann23@gmail.com>
2024-09-13 02:01:07 +0200

    feat(user profile): upload profile picture to storage bucket

--- web/src/features/userProfile/presentation/UserProfile.tsx
+++ web/src/features/userProfile/presentation/UserProfile.tsx
@@ -1,6 +1,8 @@
+import { STORAGE_BUCKET } from '@/common/constants/storageBucket.constants'
 import { useUserStore } from '@/stores/useUserStore.tsx'
+import { supabase } from '@/supabase'
 import { UploadOutlined } from '@ant-design/icons'
-import { Badge, Card, Descriptions, DescriptionsProps, Flex } from 'antd'
+import { Badge, Card, Descriptions, DescriptionsProps, Flex, message } from 'antd'
 import Avatar from 'antd/es/avatar/avatar'
 import React from 'react'
 
@@ -11,6 +13,10 @@ export const UserProfile: React.FC = () => {
   if (!userProfile) {
     return null
   }
+
+  const bucketName = STORAGE_BUCKET.UserData
+  const bucketFilePath = `${userProfile.user_id}/avatar.jpg`
+
   const handleBadgeClick = () => {
     // Trigger the file input click
     if (fileInputRef.current) {
@@ -18,11 +24,21 @@ export const UserProfile: React.FC = () => {
     }
   }
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
-    if (file) {
-      // Handle the selected file (e.g., upload to server or update avatar URL)
-      console.log('Selected file:', file)
+    if (!file) {
+      return
+    }
+    const { data, error } = await supabase.storage.from(bucketName).upload(bucketFilePath, file, {
+      cacheControl: '3600',
+      upsert: false,
+    })
+    if (error) {
+      message.error('Fehler beim Hochladen der Datei')
+      return
+    }
+    if (data) {
+      message.success('Profilbild wurde erfolgreich hochgeladen')
     }
   }
 
@@ -49,7 +65,7 @@ export const UserProfile: React.FC = () => {
           ref={fileInputRef}
           style={{ display: 'none' }}
           onChange={handleFileChange}
-          accept="image/*" // Optional: Restrict to image files
+          accept="image/jpeg"
         />
       </Card>
     </Flex>
