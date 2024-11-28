64272aa0b76445b5decc90329887fec9bb608c10
Leon Bergmann <93575165+TeePlunder@users.noreply.github.com>
2024-11-28 17:49:46 +0100

    Bessere Icons für den Edt Input (#51)
    * feat(emoji mart): install
    
    * feat(edt input): add picker
    
    * feat(edt input): pass emoji to edt input
    
    * update bun log
    
    * feat(edt input): add icon button
    
    * feat(edt input): display icon picker at the left side of the card & change icon button on open

--- web/src/features/createPoll/presentation/EdtInput.tsx
+++ web/src/features/createPoll/presentation/EdtInput.tsx
@@ -1,5 +1,8 @@
 import { Profile } from '@/common/types/tables/profiles/profile.type'
-import { Avatar, Card, Flex } from 'antd'
+import { CloseOutlined, SmileOutlined } from '@ant-design/icons'
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+import { Avatar, Button, Card, Flex, Tooltip, theme } from 'antd'
 import Input from 'antd/es/input/Input'
 import React, { useImperativeHandle } from 'react'
 
@@ -15,6 +18,11 @@ export type EdtInputHandle = {
 export const EdtInput = React.forwardRef<EdtInputHandle, EdtInputProps>((props, ref) => {
   const { profile } = props
   const [edtInput, setEdtInput] = React.useState('')
+  const [showEmojiSelection, setShowEmojiSelection] = React.useState(false)
+
+  const {
+    token: { colorErrorText },
+  } = theme.useToken()
 
   const handleEdtInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEdtInput(event.target.value)
@@ -24,12 +32,34 @@ export const EdtInput = React.forwardRef<EdtInputHandle, EdtInputProps>((props, 
     getEdtInput: () => ({ edt: edtInput, user_id: profile.user_id }),
   }))
 
+  const handleEmojiSelection = (emoji: { native: string }): void => {
+    setEdtInput(edtInput + emoji.native)
+  }
+
   return (
-    <Card style={{ width: '300px' }}>
-      <Flex vertical gap={'middle'}>
-        <Card.Meta avatar={<Avatar src={profile.avatar_url} />} title={profile.username} />
-        <Input value={edtInput} onChange={handleEdtInputChange} />
-      </Flex>
-    </Card>
+    <div style={{ position: 'relative', display: 'inline-block' }}>
+      <Card style={{ width: '300px' }}>
+        <Flex vertical gap={'middle'}>
+          <Card.Meta avatar={<Avatar src={profile.avatar_url} />} title={profile.username} />
+          <Flex gap={'small'}>
+            <Tooltip title="tolle icons :)">
+              <Button
+                onClick={() => {
+                  setShowEmojiSelection(!showEmojiSelection)
+                }}
+                shape={'circle'}
+                icon={showEmojiSelection ? <CloseOutlined style={{ color: colorErrorText }} /> : <SmileOutlined />}
+              />
+            </Tooltip>
+            <Input value={edtInput} onChange={handleEdtInputChange} />
+          </Flex>
+        </Flex>
+      </Card>
+      {showEmojiSelection && (
+        <div style={{ position: 'absolute', left: '-355px', top: '0' }}>
+          <Picker data={data} onEmojiSelect={handleEmojiSelection} theme={'dark'} locale={'de'} />
+        </div>
+      )}
+    </div>
   )
 })
